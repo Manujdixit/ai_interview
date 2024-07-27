6fc2a51af3492f2147d3f4ef16ffed26d991afbf
Manujdixit <manujdixit04@gmail.com>
2024-07-27 22:52:37 +0530

    home

--- app/page.js
+++ app/page.js
@@ -3,11 +3,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      Go to dashboard
+    <div className="h-screen w-screen flex flex-col items-center justify-center gap-5 bg-black">
       <Link href={"/dashboard"}>
-        <Button>Button</Button>
+        <Button>Go to dashboard</Button>
       </Link>
-    </>
+      <h1 className="text-red-500">
+        Note- Works best on Chrome. No dark mode ahead!!
+      </h1>
+    </div>
   );
 }
